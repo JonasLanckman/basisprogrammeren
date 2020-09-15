33f634a4558f8365ab54d61d01df86c479b99468
Jonas Lanckman <jonas-lanckman@hotmail.com>
2020-09-15 16:25:46 +0200

    15/09

---
+++ python/ind_oef/rubriekiteraties/oef_01.py
@@ -0,0 +1,10 @@
+
+
+# VARIABELEN
+ster = "*"
+
+aantalSterren = int(input ("Hoeveel sterren geef je: "))
+print (ster)
+for x in range (1, aantalSterren):
+	ster = ster + "*"
+	print (ster)
---
+++ python/ind_oef/rubriekiteraties/oef_04.py
@@ -0,0 +1,12 @@
+
+
+reeks = ""
+
+for x in range(1,100):
+
+	reeks = reeks + str(x) + ","
+	if (str(x)< 10):
+		
+
+
+print (reeks)
