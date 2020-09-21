ebd8a979ddbfb912e45e24e066ad8aae3c9c8760
Jonas Lanckman <jonas-lanckman@hotmail.com>
2020-09-21 20:58:58 +0200

    functies

---
+++ python/ind_oef/functies/oef_31.py
@@ -0,0 +1,114 @@
+
+
+# def omtrekVierkant(z):
+# 	return z*4
+
+
+# getal1=int(input("Geef een zijde in:"))
+
+# resultaat = omtrekVierkant(getal1)
+# print(resultaat)
+
+
+
+# def omtrekRechthoek(b, h):
+# 	return (b+h)*2
+
+# getal1=int(input("Geef de basis op: "))
+# getal2=int(input("Geef de hoogte op:"))
+
+# resultaat1 = omtrekRechthoek(getal1,getal2)
+# print(resultaat1)
+
+
+# def oppervlakteRechthoek(b, h):
+# 	return (b*h)
+# getal1=int(input("Geef de basis op: "))
+# getal2=int(input("Geef de hoogte op:"))
+
+# resultaat1 = oppervlakteRechthoek(getal1,getal2)
+# print(resultaat1)
+
+
+
+
+# def omtrekRuit(z):
+# 	return z*4
+
+
+# getal1=int(input("Geef een zijde in:"))
+
+# resultaat = omtrekRuit(getal1)
+# print(resultaat)
+
+
+# def oppervlakteRuit(d1,d2):
+# 	return (d1 * d2)/2
+
+# getal1 = int(input("Geef eerste diagonaal op: "))
+# getal2 = int(input("Geef tweede diagonaal op: "))
+
+# resultaat1 = oppervlakteRuit(getal1,getal2)
+# print(resultaat1)
+
+
+
+# def omtrekDriehoek(z1, z2, z3):
+# 	return(z1 + z2 + z3)
+# getal1 = int(input("Geef eerste zijde in: "))
+# getal2 = int(input("Geef tweede zijde in: "))
+# getal3 = int(input("Geef derde zijde in: "))
+
+# resultaat1 = omtrekDriehoek(getal1, getal2, getal3)
+# print(resultaat1)
+
+
+# def oppervlakteDriehoek(b, h):
+# 	return (b*h)/2
+# getal1 = int(input("Geef de basis in: "))
+# getal2 = int(input("Geef de hoogte in: "))
+
+# resultaat1 = oppervlakteDriehoek(getal1, getal2)
+# print(resultaat1)
+
+
+# def omtrekPara(l, b):
+# 	return (l + b)*2
+# getal1 = int(input("Geef de lengte in: "))
+# getal2 = int(input("Geef de breedte in: "))
+
+
+# resultaat1 = omtrekPara(getal1, getal2)
+# print(resultaat1)
+
+# def oppervlaktePara(b, h):
+# 	return (b * h)
+# getal1 = int(input("Geef de breedte in: "))
+# getal2 = int(input("Geef hoogte in: "))
+
+# resultaat1 = oppervlaktePara(getal1, getal2)
+# print(resultaat1)
+
+# import math
+
+# def omtrekCirkel(pi, d):
+# 	return (pi*d)
+# getal1 = math.pi
+# getal2 = int(input("Geef de diameter in: "))
+
+# resultaat1 = omtrekCirkel(getal1,getal2)
+# print(resultaat1)
+
+# import math
+
+# def oppervlakteCirkel(s, pi):
+# 	return (s**2*pi)
+# getal1 = int(input("Geef de straal in: "))
+# getal2 = math.pi
+# resultaat1 = oppervlakteCirkel(getal1,getal2)
+# print(resultaat1)
+
+
+
+
+
